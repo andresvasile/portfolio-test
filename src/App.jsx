69011d13bc07f0f9832b739fc8f37b1dc947b40e
Andres Vasile <60723438+andresvasile@users.noreply.github.com>
2023-12-24 05:53:57 +0000

    add personal info

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Andres Vasile",
+  title: "Software developer",
+  email: "andresvasile.av@gmail.com",
+  gitHub: "andresvasile",
+  instagram: "andresvasile/",
+  linkedIn: "andres-vasile-11469a178/",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,16 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Disney API",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "A comprehensive API for managing a fictional universe, providing features for user authentication, registration, and managing fictional characters. The authentication system uses JWT tokens and SendGrid for email notifications.",
+    url: "https://github.com/andresvasile/ApiDisney",
   },
   {
-    title: "Web Development for Beginners",
+    title: "FicticiaAPI",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
-  },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "This API manages client-related operations for the FicticiaAPI, interacting with the underlying data through the IClienteRepository interface. It supports CRUD operations for clients and provides additional functionality for managing client-specific diseases.",
+    url: "https://github.com/andresvasile/FicticiaAPI",
   },
 ];
 
